3ee9f458cee5db092c07629ab696daecd16bd546
nathanael-g-durst <nathanael.duerst@etu.unige.ch>
2021-03-10 18:23:32 +0100

    Update test.R
    Saving Adel's code for exercise 2

--- test.R
+++ test.R
@@ -1,11 +1,21 @@
-#Test GitHub
+#Adel's commit
 
-#Thank you 
+library(quantmod)
+library(zoo)
 
-#It worked !
+# (2) Fetch data regarding Apple (AAPL), Cisco (CSCO), Intel Corporation (INTC), Home Depot (HD), Alphabet (GOOG), and JPMorgan Chase (JPM) stocks in the period between 2020-01-01 to 2021-01-01 using the package quantmod.
 
+ticker <- c("AAPL", "CSCO", "INTC", "HD", "GM", "GOOG", "JPM")
+getSymbols.yahoo(ticker, env=globalenv(), from="2020-01-01", to = "2021-01-01")
 
+#(3) Plot the closing price of AAPL and CSCO over the period between 2020-01-01 to 2021-01-01.
 
-#Is it working? -Eric
+data.raw <- merge.zoo(Cl(AAPL), Cl(CSCO))
+plot(data.raw)
 
-#It is Eric ! - Nath
+#(4) Plot a candle chart of GOOG and JPM over the last 3 months of 2020.
+
+ticker1 <- c("GOOG", "JPM")
+getSymbols.yahoo(ticker1, env=globalenv(), from="2020-10-01", to = "2020-12-31")
+data.raw1 <- merge.zoo(Cl(AAPL), Cl(CSCO))
+candleChart(data.raw1)
